14d44f97336952737148649b0055211725f65670
ValerijaDrozda <99147124+ValerijaDrozda@users.noreply.github.com>
2023-02-19 22:31:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
-# python3
+## Valerija Drozda 221RDB450
 
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
-
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
-
 
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            opening_brackets_stack.append(next)
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
-
+            if((len(opening_brackets_stack) == 0) or not (are_matching(opening_brackets_stack[-1], next))):
+                return i+1
+            opening_brackets_stack.pop()
+    return 0
+    
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
+    choice = input("Choose input: ")
+    if "F" in choice:
+        path = input("Enter path to file: ")
+        with open(path, "r") as file:
+            text = file.read()
+            mismatch = find_mismatch(text)
+            if mismatch == 0:
+                print("Success")
+            else:
+                print(mismatch)
+    elif "I" in choice:
+        text = input()
+        mismatch = find_mismatch(text)
+        if mismatch == 0:
+            print("Success")
+        else:
+            print(mismatch)
+    else:
+        print("Invalid input choice.")
 
 if __name__ == "__main__":
     main()
